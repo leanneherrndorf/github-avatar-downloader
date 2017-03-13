4b7bdd45ae1689324b2f6ce477335080a89cc9ea
Leanne <leanneherrndorf@gmail.com>
2017-03-13 23:21:43 +0000

    Modified download_avatars to get Repo Contributors

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,12 @@
 var request = require('request');
+var GITHUB_USER = "leanneherrndorf";
+var GITHUB_TOKEN = "9f4b43ca9e916d6914b8682baab62f6551be9455";
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {

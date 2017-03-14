9ca19ecdd0dae119a57cc569ae44a3f7c7976ebe
Leanne <leanneherrndorf@gmail.com>
2017-03-14 00:59:34 +0000

    Modified download_avatars to download the image of each avatar URL

--- download_avatars.js
+++ download_avatars.js
@@ -2,6 +2,8 @@ var request = require('request');
 var fs = require('fs');
 var GITHUB_USER = "leanneherrndorf";
 var GITHUB_TOKEN = "9f4b43ca9e916d6914b8682baab62f6551be9455";
+var avatarURL = [];
+var loginVAL = {};
 
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -29,7 +31,31 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
 
   for( var x in result) {
-    console.log("Avatar URL: ", result[x].avatar_url);
+    loginVAL = "avatars/" + result[x].login + ".jpg";
+    avatarURL = result[x].avatar_url;
+    downloadImageByURL(avatarURL, loginVAL);
   }
 });
+
+
+function downloadImageByURL(url, filePath) {
+
+  request.get(url)               // Note 1
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .on('end', function (end) {
+        console.log('Downloading image...');
+      })
+       .pipe(fs.createWriteStream(filePath))
+
+       .on('finish', function(finish) {
+          console.log('Download complete.');
+       });
+
+}
+
 

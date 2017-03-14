3ba6976b49de5ded01e453a44f1b27f7339706ce
Leanne <leanneherrndorf@gmail.com>
2017-03-14 02:43:11 +0000

    Modified download_avatars to create avatar file if not already existing, and commented code

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,9 @@
+//Leanne Herrndorf
+//Lighthouse Labs Project 1
+//
+//This project requests avatars for all contributers to a given project on GitHub, and downloads them to disk.
+
+
 var request = require('request');
 var fs = require('fs');
 var GITHUB_USER = "leanneherrndorf";
@@ -9,6 +15,60 @@ var repo = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+//Construct URL, return an array of contributors, call callback function
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, response, body) {
+    if (err) {
+      throw err;
+    }
+    if (response && response.statusCode !== 200) {
+      console.log("Response was not 200!", response);
+      return false;
+    }
+
+    var result = JSON.parse(body);
+    cb(err, result);
+  });
+}
+
+//Download image with input url and outputs to desired download filePath
+function downloadImageByURL(url, filePath) {
+
+  //if avatars folder does not yet exist, create it
+  if(!fs.existsSync("./avatars")) {
+    fs.mkdirSync("./avatars");
+  }
+  request.get(url)
+        .on('error', function (err) {
+          throw err;
+        })
+
+       .on('response', function (response) {
+         //Notify user if a response code other than 200 is received
+         if (response && response.statusCode !== 200) {
+           console.log("Response was not 200!", response);
+           return false;
+         }
+       })
+       //Notify user that image is downloading
+       .on('end', function (end) {
+         console.log('Downloading image...');
+       })
+       //Notify user that image is finished downloading
+       .pipe(fs.createWriteStream(filePath))
+       .on('finish', function(finish) {
+         console.log('Download complete.');
+       });
+}
 
 //Ensure owner and user are passed in command line argument, if not request in console.
 if( owner && repo) {
@@ -26,59 +86,8 @@ if( owner && repo) {
   console.log('Please input owner and repo.');
 }
 
-//Construct URL, return an array of contributors
-function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  //console.log(requestURL);
 
-  var options = {
-    url: requestURL,
-    headers: {
-      'User-Agent': 'request'
-    }
-  };
 
-  //retun array of contributors, call callback function
-  request(options, function(err, response, body) {
-    if (err) throw err;
 
-    if (response && response.statusCode !== 200) {
-      console.log("Response was not 200!", response);
-      return false;
-    }
-    //console.log('Response Status Code:', response.statusCode);
-    //Constructs an array of contributors
-    var result = JSON.parse(body);
-
-    //Call callback function
-    cb(err, result);
-  });
-}
-
-//Download image for desired avatar url
-function downloadImageByURL(url, filePath) {
-  request.get(url)
-       .on('error', function (err) {
-         throw err;
-       })
-       //Notify user if a response code other than 200 is received
-       .on('response', function (response) {
-        //console.log('Response Status Code: ', response.statusCode);
-          if (response && response.statusCode !== 200) {
-            console.log("Response was not 200!", response);
-          return false;
-          }
-       })
-       //Notify user that image is downloading
-       .on('end', function (end) {
-        console.log('Downloading image...');
-      })
-       //Notify user that image is finished downloading
-       .pipe(fs.createWriteStream(filePath))
-       .on('finish', function(finish) {
-          console.log('Download complete.');
-       });
-
-}
 
 

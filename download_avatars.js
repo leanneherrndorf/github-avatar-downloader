011aabbeff17169a3f4d37aea0d653c7b9efb48f
Leanne <leanneherrndorf@gmail.com>
2017-03-14 00:26:12 +0000

    Modified download_avatar.js to print out each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -20,35 +20,16 @@ function getRepoContributors(repoOwner, repoName, cb) {
   request(options, function(err, response, body) {
     if (err) throw err;
     console.log('Response Status Code:', response.statusCode);
-
-    console.log('Body: ', JSON.parse(body));
+    var result = JSON.parse(body);
+    cb(err, result);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+
+  for( var x in result) {
+    console.log("Avatar URL: ", result[x].avatar_url);
+  }
 });
 
-
-// function downloadImageByURL(url, filePath) {
-
-//   request.get(url)               // Note 1
-//        .on('error', function (err) {                                   // Note 2
-//          throw err;
-//        })
-//        .on('response', function (response) {                           // Note 3
-//          console.log('Response Status Code: ', response.statusCode);
-//        })
-//        .on('end', function (end) {
-//         console.log('Downloading image...');
-//       })
-//        .pipe(fs.createWriteStream(filePath))
-
-//        .on('finish', function(finish) {
-//           console.log('Download complete.');
-//        });
-
-// }
-
-// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
